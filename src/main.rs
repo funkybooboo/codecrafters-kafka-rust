71680e21e7a8ed92789dcaf96fe9bfedd27e17c2
Nate Stott <nate.stott@pm.me>
2025-03-02 15:44:35 -0500

    write to stream

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #![allow(unused_imports)]
+
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
+                let buf: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 7];
+                _stream.write(&buf).unwrap();
                 println!("accepted new connection");
             }
             Err(e) => {
