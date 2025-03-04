bf9895334d0e1395a1b0e3c8421e6cb0e24052ce
Nate Stott <nate.stott@pm.me>
2025-03-03 22:34:50 -0500

    end back an error message for the api verison

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::net::{TcpListener, TcpStream};
 use bytes::{Buf, BufMut};
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut request = read_request(&mut stream);
+    let request = read_request(&mut stream);
 
-    let _request_api_key = request.get_i16();
-    let _request_api_version = request.get_i16();
-    let correlation_id = request.get_i32();
+    let _request_api_key = request.as_slice().get_i16();
+    let _request_api_version = request.as_slice().get_i16();
+    let correlation_id = request.as_slice().get_i32();
 
     let mut response = Vec::with_capacity(8);
     response.put_i32(0);
@@ -17,13 +17,13 @@ fn handle_connection(mut stream: TcpStream) {
     stream.write_all(&response).unwrap();
 }
 
-fn read_request(stream: &mut TcpStream) -> &[u8] {
+fn read_request(stream: &mut TcpStream) -> Vec<u8> {
     let request_length = read_request_length(stream);
 
     let mut request_buffer = vec![0; request_length];
     stream.read_exact(&mut request_buffer).unwrap();
 
-    request_buffer.as_slice()
+    request_buffer
 }
 
 fn read_request_length(stream: &mut TcpStream) -> usize {
